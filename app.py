f11f1fdd8a43d4e86a0bc59be5e354b1bdfc31dc
mohamed shahat <52858326+shiky8@users.noreply.github.com>
2023-11-21 15:10:17 +0200

    Add files via upload
    API

---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask
+app = Flask(__name__)
+from functools import wraps
+from flask import request, abort,jsonify
+import ssl
+import json
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('host.cert', 'host.key')
+read_cv = open("cv2.json")
+my_cv=json.load(read_cv)
+# The actual decorator function
+def require_appkey(view_function):
+    @wraps(view_function)
+    # the new, post-decoration function. Note *args and **kwargs here.
+    def decorated_function(*args, **kwargs):
+        with open('api.key', 'r') as apikey:
+            key=apikey.read().replace('\n', '')
+        if request.args.get('key') and request.args.get('key') == key:
+            return view_function(*args, **kwargs)
+        # print("key = ",key)
+        if request.headers.get('x-api-key') and request.headers.get('x-api-key') == key:
+            return view_function(*args, **kwargs)
+        else:
+            abort(401)
+    return decorated_function
+
+
+@app.route('/cv/')
+@require_appkey
+def cv():
+    return jsonify(my_cv)
+
+@app.route('/personal_info/', methods=['GET'])
+@require_appkey
+def personal_info():
+    return jsonify(my_cv[0]["personal_info"])
+
+@app.route('/links/', methods=['GET'])
+@require_appkey
+def links():
+    return jsonify(my_cv[0]["links"])
+
+@app.route('/objective/', methods=['GET'])
+@require_appkey
+def objective():
+    return jsonify(my_cv[0]["objective"])
+
+@app.route('/skills/', methods=['GET'])
+@require_appkey
+def skills():
+    return jsonify(my_cv[0]["skills"])
+
+@app.route('/Soft_skills/', methods=['GET'])
+@require_appkey
+def Soft_skills():
+    return jsonify(my_cv[0]["Soft_skills"])
+
+@app.route('/progamming/', methods=['GET'])
+@require_appkey
+def progamming():
+    return jsonify(my_cv[0]["progamming language"])
+
+@app.route('/language/', methods=['GET'])
+@require_appkey
+def language():
+    return jsonify(my_cv[0]["language"])
+
+@app.route('/Awards/', methods=['GET'])
+@require_appkey
+def Awards():
+    return jsonify(my_cv[0]["Awards"])
+
+@app.route('/education/', methods=['GET'])
+@require_appkey
+def education():
+    return jsonify(my_cv[0]["education"])
+
+@app.route('/certificates/', methods=['GET'])
+@require_appkey
+def certificates():
+    return jsonify(my_cv[0]["certificates"])
+
+@app.route('/experience/', methods=['GET'])
+@require_appkey
+def experience():
+    return jsonify(my_cv[0]["experience"])
+
+@app.route('/project/', methods=['GET'])
+@require_appkey
+def project():
+    return jsonify(my_cv[0]["project"])
+
+@app.route('/Badges/', methods=['GET'])
+@require_appkey
+def Badges():
+    return jsonify(my_cv[0]["Badges"])
+
+
+app.run(host='127.0.0.1', debug = False/True, ssl_context=context)
---
+++ rtestreq.py
@@ -0,0 +1,115 @@
+import requests
+
+
+# View Full Resume
+url5 = 'https://127.0.0.1:5000/cv/'
+x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+print(x5.text)
+
+print(x5.status_code)
+
+# View personal_info
+# url5 = 'https://127.0.0.1:5000/personal_info/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View links
+# url5 = 'https://127.0.0.1:5000/links/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View objective
+# url5 = 'https://127.0.0.1:5000/objective/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View skills
+# url5 = 'https://127.0.0.1:5000/skills/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View Soft_skills
+# url5 = 'https://127.0.0.1:5000/Soft_skills/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View progamming language
+# url5 = 'https://127.0.0.1:5000/progamming/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View language
+# url5 = 'https://127.0.0.1:5000/language/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View Awards
+# url5 = 'https://127.0.0.1:5000/Awards/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View education
+# url5 = 'https://127.0.0.1:5000/education/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View certificates
+# url5 = 'https://127.0.0.1:5000/certificates/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View experience
+# url5 = 'https://127.0.0.1:5000/experience/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View project
+# url5 = 'https://127.0.0.1:5000/project/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
+# View Badges
+# url5 = 'https://127.0.0.1:5000/Badges/'
+# x5 = requests.get(url5 , headers = {'x-api-key': 'eiWee8ep9due4deeshoa8Peichai8Eih'},verify=False)
+
+# print(x5.text)
+
+# print(x5.status_code)
+
